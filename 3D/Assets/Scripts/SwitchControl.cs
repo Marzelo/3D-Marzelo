ec0f7c40249d0f4ab2d8393f757a1ce3a694efba
Marzelo <marzelozorogastuan@gmail.com>
2018-06-19 20:34:25 -0500

    ba

--- 3D/Assets/Scripts/SwitchControl.cs
+++ 3D/Assets/Scripts/SwitchControl.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchControl : MonoBehaviour {
 
 	public Transform platform;
-	public Vector3 targetPoint;
+    public Vector3 targetPoint;
 	public float transitionSpeed;
+
+    bool _enabled;
+    public bool startEnabled;
+    public bool isEnabled { get { return _enabled; }}
+    public Color enabledColor;
+    public Color disabledColor;
+    Renderer objectRenderer;
+
+    public SwitchControl alternateSwitch;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        objectRenderer = GetComponent<Renderer>();
+        SetEnable(startEnabled);
 	}
 	
 	// Update is called once per frame
@@ -17,16 +29,32 @@ public class SwitchControl : MonoBehaviour {
 		
 	}
 
-	public void Activate () {
-		Debug.Log ("Activated" + name);
+    public void SetEnable (bool enabledState) {
+        if (alternateSwitch != null) { alternateSwitch.SetEnable(enabledState); }
+        objectRenderer.material.color = enabledState ? enabledColor : disabledColor; 
+        _enabled = enabledState;
+    }
 
-		StartCoroutine (MovePlatformToTargetPoint ());
+    public void SayDebug (bool canSay) {
+        Debug.Log(canSay);
+    }
+
+	public void Activate () {
+        if (isEnabled){
+            Debug.Log("Activated" + name);
+            StartCoroutine(MovePlatformToTargetPoint());
+            SetEnable(false);
+        } 
 	}
-	IEnumerator MovePlatformToTargetPoint () {
-		while (platform != targetPoint) {
+    IEnumerator MovePlatformToTargetPoint() {
+        Vector3 nextTarget = platform.position;
+        while (platform.position != targetPoint) {
             platform.position = Vector3.MoveTowards (platform.position, targetPoint, transitionSpeed * Time.deltaTime);
 		    yield return null;
 	    }
+        targetPoint = nextTarget;
+        if (alternateSwitch != null) { alternateSwitch.targetPoint = nextTarget; }
+        SetEnable(true);
 		yield return null;
 	}
 }
