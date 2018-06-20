3257709090bec145ea1f2fc2dc43f4eb46bc688b
Marzelo <marzelozorogastuan@gmail.com>
2018-06-19 19:28:45 -0500

    no guardado

---
+++ 3D/Assets/Scripts/PlataformerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformerMovement : MonoBehaviour {
+
+
+	public float horizontalSpeed;
+	Vector3 movement;
+	public Rigidbody rigidbody3D;
+	public float impulseValue;
+	public SwitchControl currentSwitch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		movement = transform.position;
+		float horizontalDirection = Input.GetAxis("Horizontal");
+		float verticalDirection = Input.GetAxis("Vertical");
+		if (Input.GetKey(KeyCode.J)) {
+			transform.Rotate(Vector3.up *100f * Time.deltaTime);
+		}
+		if (horizontalDirection !=0) {
+			movement += Vector3.right * horizontalDirection * horizontalSpeed * Time.deltaTime;
+		}
+		if (verticalDirection !=0) {
+			movement += Vector3.forward * verticalDirection * horizontalSpeed * Time.deltaTime;
+		}
+
+		rigidbody3D.MovePosition(movement);
+	}
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rigidbody3D.AddForce(Vector3.up * impulseValue, ForceMode.Impulse);
+		}
+		if (currentSwitch != null && Input.GetKeyDown (KeyCode.E)) {
+			currentSwitch.Activate ();
+		} 
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Switch")) {
+			currentSwitch = other.GetComponent<SwitchControl> ();
+		}
+	}
+	void OnTriggerExit (Collider other) {
+		if (other.CompareTag ("Switch")) {
+			currentSwitch = null;
+		}
+	}
+}
---
+++ 3D/Assets/Scripts/SwitchControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchControl : MonoBehaviour {
+
+	public Transform platform;
+	public Vector3 targetPoint;
+	public float transitionSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Activate () {
+		Debug.Log ("Activated" + name);
+
+		StartCoroutine (MovePlatformToTargetPoint ());
+	}
+	IEnumerator MovePlatformToTargetPoint () {
+		while (platform != targetPoint) {
+            platform.position = Vector3.MoveTowards (platform.position, targetPoint, transitionSpeed * Time.deltaTime);
+		    yield return null;
+	    }
+		yield return null;
+	}
+}
