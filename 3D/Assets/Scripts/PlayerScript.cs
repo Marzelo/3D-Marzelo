c075708d65ba29b7db87dc32db2ab2e4f64d0a10
Marzelo <marzelozorogastuan@gmail.com>
2018-06-28 22:07:20 -0500

    new

--- 3D/Assets/Scripts/PlayerScript.cs
+++ 3D/Assets/Scripts/PlayerScript.cs
@@ -21,7 +21,7 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (currentHP == 0){
+        if (currentHP <= 0){
             int currentIndex = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentIndex);
         }
